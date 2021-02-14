0b8e3e67375d9a3d2f7acb5992f947f8ea65a971
tretikoff <tretikov.konstantin@studiomobile.ru>
2021-02-14 09:38:32 +0300

    Add linter

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val ktlint by configurations.creating
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+
+group = "me.professional"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    ktlint("com.pinterest:ktlint:0.40.0")
+}
+
+val outputDir = "${project.buildDir}/reports/ktlint/"
+val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+val ktlintCheck by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("src/**/*.kt")
+}
+
+val ktlintFormat by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("-F", "src/**/*.kt")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
